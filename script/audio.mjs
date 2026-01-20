db6af95da40c9047031f775cb280422d10f82461
NovAldesign <williams.lavon@gmail.com>
2026-01-19 22:42:12 -0500

    create track title from api

--- script/audio.mjs
+++ script/audio.mjs
@@ -4,4 +4,8 @@ export function createAudio(track) {
     container.style.padding = '15px';
     container.style.marginTop = '10px';
 
- 
+ const title = document.createElement('strong');
+    // Using track.name from Jamendo API
+    title.textContent = `🎵 ${track.name}`;
+    title.style.display = 'block';
+    title.style.marginBottom = '8px';

4eae5c4cc1f81ff32d1708f90966b43eabe330b3
NovAldesign <williams.lavon@gmail.com>
2026-01-19 22:27:54 -0500

    creating export function

---
+++ script/audio.mjs
@@ -0,0 +1,7 @@
+export function createAudio(track) {
+    const container = document.createElement('div');
+    container.className = 'record';
+    container.style.padding = '15px';
+    container.style.marginTop = '10px';
+
+ 
